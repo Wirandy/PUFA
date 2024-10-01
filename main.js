bcdf3e49fb3a263154a3097bdd2aaf1a9f115980
wirandy sitinjak <sitinjakwirandy@gmail.com>
2024-10-01 23:39:05 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuIcon = document.getElementById("icon-u");
+const menuList = document.getElementById("menu-list");
+
+menuIcon.addEventListener("click", () =>{
+    menuList.classList.toggle("hidden");
+   
+});
